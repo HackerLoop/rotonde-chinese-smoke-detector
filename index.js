7ed95a7f89a51bac317fd9dc1bbe220954f3d26f
agricool-machine <admin@agricool.co>
2016-03-15 14:58:44 +0000

    ..

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const fetch = require('node-fetch');
 const client = require('rotonde-client/node/rotonde-client')('ws://rotonde:4224');
 
 client.eventHandlers.attach('SMOKE_DETECTOR', () => {
-  fetch('');
 });
 
 client.connect();
